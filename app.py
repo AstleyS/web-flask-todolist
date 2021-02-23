569e0b22796daea580ad2076fd0cdabad643cd23
Astley Santos <a21704176@alunos.ulht.pt>
2021-02-23 00:29:02 +0000

    update

--- app.py
+++ app.py
@@ -3,21 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(100), nullable = False)
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
 
     def __repr__(self):
-        return '<Task %r>' % self.id
+        return f'<Task {self.id}>' 
     
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('todo/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
