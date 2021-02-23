f6a7a5f0a3d98ab6e290b302d51c134a277aba73
Astley Santos <a21704176@alunos.ulht.pt>
2021-02-23 18:14:16 +0000

    Added missing static file

--- app.py
+++ app.py
@@ -30,5 +30,6 @@ def index():
         todos = Todo.query.order_by(Todo.date_created).all()
         return render_template('todo/index.html', todos = todos)
 
+
 if __name__ == '__main__':
     app.run(debug=True)
